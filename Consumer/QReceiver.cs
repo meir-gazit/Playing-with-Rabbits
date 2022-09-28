f522c5ff7443ad07e9cabbe140b613e9610b1d49
User <User@GAZIT-NETWORK>
2022-09-28 09:34:25 +0300

    Add project files.

---
+++ Consumer/QReceiver.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var host = "localhost";
+var factory = new ConnectionFactory() { HostName = host };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("QueueTest", false, false, false, null);
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray(); ;
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Recive message {0}...", message);
+    };
+
+    Console.WriteLine("Press to Exit.");
+    Console.ReadLine();
+}
---
+++ Producer/QSender.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var host = "localhost";
+var factory = new ConnectionFactory() { HostName = host };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("QueueTest", false, false, false, null);
+    var message = "Test Queue Successful";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "QueueTest", null, body);
+    Console.WriteLine("Sent message {0}...", message);
+    Console.WriteLine("Press to Exit.");
+    Console.ReadLine();
+}
